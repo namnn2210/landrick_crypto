562c8e450a9960b62b6924dd2a177a3fe6d6d2e7
namng <nam.nn@bidgear.com>
2023-12-24 17:01:51 +0700

    'edit'

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from .models import BlogModel
+from tinymce.widgets import TinyMCE
+from django.db import models
+from django import forms
 
 
 # Register your models here.
+class BlogModelAdminForm(forms.ModelForm):
+    class Meta:
+        model = BlogModel  # Replace with your actual model name
+        fields = ['thumb','title', 'description', 'content', 'status']
+        widgets = {
+            'content': TinyMCE(attrs={'cols': 80, 'rows': 30}),
+        }
+
+
 @admin.register(BlogModel)
 class WalletAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'content', 'status', 'created_at', 'updated_at')
     list_filter = ('status',)
     search_fields = ('title',)
+
+    form = BlogModelAdminForm
