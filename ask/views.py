e43334951dc6b6d6d607fa876309c0e500885412
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-25 14:44:24 +0700

    edit

--- ask/views.py
+++ ask/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import AskModel
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def ask(request):
     list_asks = AskModel.objects.filter(status=1)
     return render(request=request, template_name='crypto-ask.html', context={'list_ask': list_asks})
 
-
+@login_required(login_url='login')
 def add_ask(request):
     if request.method == 'POST':
         ask_input = request.POST.get('ask', '')
