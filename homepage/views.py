d22f8eb1c004b31fc21c337335e9e5675a1ae99f
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-25 11:51:40 +0700

    edit

--- homepage/views.py
+++ homepage/views.py
@@ -27,6 +27,30 @@ def get_listing_latest(count=10):
         return list_format_latest
     return None
 
+def get_listing_change():
+    list_format_latest = []
+    api_key = 'ace6f6a9-d2fb-410e-b1c1-ef3e904e329d'
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    headers = {
+        'X-CMC_PRO_API_KEY': api_key,
+    }
+    parameters = {
+        'start': '1',
+        'limit': 4,  # Replace 'BTC' with the symbol of the cryptocurrency you want to get
+        'sort': 'percent_change_24h',  # Replace 'USD' with the currency you want to convert to
+    }
+    response = requests.get(url, params=parameters, headers=headers)
+    if response.status_code == 200:
+        data = response.json()['data']
+        for item in data:
+            list_format_latest.append({'id': item['id'], 'name': item['name'], 'symbol': item['symbol'],
+                                       'price': item['quote']['USD']['price'],
+                                       'volume_change_24h': item['quote']['USD']['volume_change_24h'],
+                                       'percent_change_24h': item['quote']['USD']['percent_change_24h'],
+                                       'market_cap': item['quote']['USD']['market_cap']})
+        return list_format_latest
+    return None
+
 def get_btc_price():
     # Replace 'YOUR_API_KEY' with your actual CoinMarketCap API key
     api_key = 'ace6f6a9-d2fb-410e-b1c1-ef3e904e329d'
@@ -69,12 +93,16 @@ def about(request):
 
 def market(request):
     list_format_latest = get_listing_latest(30)
+    list_change = get_listing_change()
     if list_format_latest:
         items_per_page = 10
         paginator = Paginator(list_format_latest, items_per_page)
         page_number = request.GET.get('page')
         page = paginator.get_page(page_number)
-        return render(request=request, template_name='crypto-market.html', context={'page': page})
+
+
+
+        return render(request=request, template_name='crypto-market.html', context={'page': page,'list_change':list_change})
     return render(request=request, template_name="index.html", context={'list_format_latest': None})
 
 
