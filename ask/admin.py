962dd8cd134914f113ebb7f85891f7f8e7072941
Ngo Ngoc Nam <namngocngo22@gmail.com>
2023-12-27 10:17:08 +0700

    edit

--- ask/admin.py
+++ ask/admin.py
@@ -7,7 +7,7 @@ from django import forms
 class AskModelAdminForm(forms.ModelForm):
     class Meta:
         model = AskModel  # Replace with your actual model name
-        fields = ['user', 'ask', 'answer', 'content', 'status']
+        fields = ['user', 'slug', 'ask', 'answer', 'content', 'status']
         widgets = {
             'content': TinyMCE(attrs={'cols': 80, 'rows': 30}),
         }
@@ -16,7 +16,7 @@ class AskModelAdminForm(forms.ModelForm):
 # Register your models here.
 @admin.register(AskModel)
 class AskAdmin(admin.ModelAdmin):
-    list_display = ('user', 'ask', 'answer', 'status', 'created_at', 'updated_at')
+    list_display = ('user', 'slug', 'ask', 'answer', 'status', 'created_at', 'updated_at')
     list_filter = ('user', 'status', 'created_at',)
     search_fields = ('ask', 'answer',)
 
