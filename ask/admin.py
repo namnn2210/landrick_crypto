1734cd175d743c54662618dff0dca527a643c13c
namng <nam.nn@bidgear.com>
2024-02-14 14:30:15 +0700

    edit

--- ask/admin.py
+++ ask/admin.py
@@ -8,7 +8,7 @@ from tinymce.widgets import TinyMCE
 class AskModelAdminForm(forms.ModelForm):
     class Meta:
         model = AskModel  # Replace with your actual model name
-        fields = ['user', 'slug', 'ask', 'category', 'answer', 'content', 'status']
+        fields = ['user', 'slug', 'ask', 'category', 'answer', 'content', 'status','pinned']
 
         widgets = {
             'content': TinyMCE(attrs={'cols': 80, 'rows': 30}),
@@ -18,8 +18,8 @@ class AskModelAdminForm(forms.ModelForm):
 # Register your models here.
 @admin.register(AskModel)
 class AskAdmin(admin.ModelAdmin):
-    list_display = ('user', 'slug', 'ask', 'category', 'answer', 'status', 'created_at', 'updated_at')
-    list_filter = ('user', 'status', 'category', 'created_at',)
+    list_display = ('user', 'slug', 'ask', 'category', 'answer', 'status','pinned', 'created_at', 'updated_at')
+    list_filter = ('user', 'status', 'category', 'created_at','pinned',)
     search_fields = ('ask', 'answer',)
 
     form = AskModelAdminForm
