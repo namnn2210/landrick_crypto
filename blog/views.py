f6659ace3535759c8c3dc9f3997705805f0d210d
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-03-28 12:55:41 +0700

    edit

--- blog/views.py
+++ blog/views.py
@@ -32,22 +32,22 @@ def blog(request):
     return render(request=request, template_name='crypto-blog.html', context={'page': page})
 
 
-# def get_trending_latest():
-#     list_format_latest = []
-#     url = 'https://api.coingecko.com/api/v3/search/trending'
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         data = response.json()['coins']
-#         for item in data[:10]:
-#             price_regex = r'(?<=\$)[0-9,]+\.(?:[0-9]+|<sub title="[0-9]+\.[0-9]+">[0-9]+<\/sub>[0-9]+)'
-#             price_str = item['item']['data']['price']
-#             price_match = re.findall(price_regex, price_str)
-#             price = float(price_match[0].replace(',', ''))
-#             list_format_latest.append(
-#                 {'name': item['item']['name'], 'thumb': item['item']['thumb'], 'symbol': item['item']['symbol'],
-#                  'price': price})
-#         return list_format_latest
-#     return None
+def get_trending_latest():
+    list_format_latest = []
+    url = 'https://api.coingecko.com/api/v3/search/trending'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()['coins']
+        for item in data[:10]:
+            price_regex = r'(?<=\$)[0-9,]+\.(?:[0-9]+|<sub title="[0-9]+\.[0-9]+">[0-9]+<\/sub>[0-9]+)'
+            price_str = item['item']['data']['price']
+            price_match = re.findall(price_regex, price_str)
+            price = float(price_match[0].replace(',', ''))
+            list_format_latest.append(
+                {'name': item['item']['name'], 'thumb': item['item']['thumb'], 'symbol': item['item']['symbol'],
+                 'price': price})
+        return list_format_latest
+    return None
 
 
 def blog_detail(request, slug):
@@ -55,10 +55,10 @@ def blog_detail(request, slug):
     category = blog.category
     list_related_blogs = BlogModel.objects.filter(category=category).exclude(slug=slug)[:3]
     print('aaaaaaaaaaaaaaaaaaaa', list_related_blogs)
-    # list_trending_latest = get_listing_latest()
-    # print('===============', list_trending_latest)
+    list_trending_latest = get_listing_latest()
+    print('===============', list_trending_latest)
     return render(request, 'crypto-blog-detail.html',
-                  {'blog': blog, 'list_format_latest': [], 'list_related_blogs': list_related_blogs})
+                  {'blog': blog, 'list_format_latest': list_trending_latest, 'list_related_blogs': list_related_blogs})
 
 
 def blog_by_category(request, slug):
