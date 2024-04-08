f86b0c57601d5fd81483e0efe25497f8e5e43f4a
Ngo Ngoc Nam <namngocngo22@gmail.com>
2024-04-08 21:11:31 +0700

    edit

--- feed/admin.py
+++ feed/admin.py
@@ -5,7 +5,7 @@ from .models import FeedModel, FeedCommentModel
 # Register your models here.
 @admin.register(FeedModel)
 class FeedAdmin(admin.ModelAdmin):
-    list_display = ('author', 'thumb', 'description', 'slug', 'status', 'created_at', 'updated_at')
+    list_display = ('title', 'author', 'thumb', 'description', 'slug', 'status', 'created_at', 'updated_at')
     list_filter = ('author', 'status',)
     search_fields = ('author', 'slug')
 
